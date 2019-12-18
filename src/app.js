abb0bd15e80fced09e186f4b3edb4aa374b44634
Vitor Martins <vitor.f.martins@accenture.com>
2019-12-18 11:22:54 -0300

    Refatorando tratamento de erros

--- src/app.js
+++ src/app.js
@@ -4,9 +4,7 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import cors from 'cors';
-import httpStatus from 'http-status';
 
-import APIError from './helpers/APIError';
 import indexRouter from './routes/index';
 
 const app = express();
@@ -23,9 +21,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 
-app.use((req, res, next) => {
-  const err = new APIError('API not found', httpStatus.NOT_FOUND);
-  return next(err);
-});
+app.use((req, res, next) => next());
 
 export default app;
--- src/controllers/baseController.controller.js
+++ src/controllers/baseController.controller.js
@@ -1,3 +1,5 @@
+import httpStatus from "http-status";
+
 export default {
   findAll: (router, model) => {
     router.get('/', (req, res, next) => {
@@ -6,8 +8,10 @@ export default {
         include: [{ all: true, nested: false }],
         attributes: { exclude: ['insertionUserId', 'dh_insertion'] }
       })
-        .then(content => res.status(200).json(content))
-        .catch(err => res.status(500).json({ sucess: 0, error: err }));
+        .then(content => res.status(httpStatus.OK).json(content))
+        .catch(err =>
+          res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ sucess: 0, error: err })
+        );
     });
   },
   findByPk: (router, model) => {
@@ -16,30 +20,40 @@ export default {
         include: [{ all: true, nested: true }],
         attributes: { exclude: ['insertionUserId', 'dh_insertion'] }
       })
-        .then(content => res.status(200).json(content))
-        .catch(err => res.status(500).json({ success: 0, error: err }));
+        .then(content => res.status(httpStatus.OK).json(content))
+        .catch(err =>
+          res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: 0, error: err })
+        );
     });
   },
   insert: (router, model) => {
     router.post('/', (req, res, next) => {
       model.create(req.body)
-        .then(content => res.status(200).json({ success: 1, message: 'Operation succeed', content }))
-        .catch(err => res.status(500).json({ success: 0, message: err }));
+        .then(content => res.status(httpStatus.OK).json({ success: 1, message: 'Operation succeed', content }))
+        .catch(err =>
+          res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: 0, message: err })
+        );
     });
   },
   update: (router, model) => {
     router.put('/:id', (req, res, next) => {
       return model.update({ where: { id: req.params.id } })
-        .then(content => res.status(200).json(content))
-        .catch(err => res.status(500).json({ success: 0, error: err }));
+        .then(content => res.status(httpStatus.OK).json(content))
+        .catch(err =>
+          res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: 0, error: err })
+        );
     });
   },
   delete: (router, model) => {
     router.delete('/', (req, res, next) => {
       model.findByPk(req.params.id).then(content =>
         content.destroy()
-          .then(() => res.status(200).json({ success: 1, message: 'Operation succeed' }))
-          .catch(err => res.status(500).json({ success: 0, error: err }))
+          .then(() =>
+            res.status(httpStatus.OK).json({ success: 1, message: 'Operation succeed' })
+          )
+          .catch(err =>
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: 0, error: err })
+          )
       );
     });
   }
