a4ba2b0ba581da2e16373af683ea808beb3a2cb7
Vitor Martins <vitor.f.martins@accenture.com>
2019-12-18 17:46:18 -0300

    Implementing find event by course

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -1,14 +1,27 @@
 import express from 'express';
+import httpStatus from 'http-status';
 
 import baseController from './baseController.controller';
 
-import { Event as model } from '../models';
+import { Event, EventCourse } from '../models';
 
 const router = express.Router();
 
-baseController.findAll(router, model);
-baseController.findByPk(router, model);
-baseController.insert(router, model);
-baseController.delete(router, model);
+baseController.findAll(router, Event);
+baseController.findByPk(router, Event);
+baseController.insert(router, Event);
+baseController.delete(router, Event);
+
+/* Find Events by Course */
+router.get('/find-by-course/:courseId', (req, res, next) => {
+  return EventCourse.findOne({
+    where: { courseId: req.params.courseId },
+    include: [{ all: true, nested: true }],
+  })
+    .then(content => res.status(httpStatus.OK).json(content.event))
+    .catch(err =>
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ sucess: 0, error: err })
+    );
+});
 
 export default router;
