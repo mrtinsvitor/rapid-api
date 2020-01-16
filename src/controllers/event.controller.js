455bb79444cd1cbf0f8156eda87918ed2ee8eb76
Vitor Martins <vitor.f.martins@accenture.com>
2020-01-16 14:40:15 -0300

    Implementing create event service

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -3,7 +3,7 @@ import httpStatus from 'http-status';
 
 import baseController from './baseController.controller';
 
-import { Event, EventCourse } from '../models';
+import { Event, EventCourse, sequelize } from '../models';
 
 const router = express.Router();
 
@@ -19,7 +19,33 @@ router.get('/find-by-course/:courseId', (req, res, next) => {
     );
 });
 
-/* Generic CRUD methods. Implement new methods above */ 
+/* Register a new event */
+router.post('/create-event', async (req, res, next) => {
+  try {
+    const response = await sequelize.transaction(async (t) => {
+      const newEvent = await Event.create(req.body, { transaction: t })
+        .then(content => content)
+
+      const newEventCourse = {
+        eventId: newEvent.id,
+        courseId: req.body.courseId,
+        insertionUserId: req.body.insertionUserId
+      };
+
+      await EventCourse.create(newEventCourse, { transaction: t })
+        .then(content => content)
+        
+      return newEvent;
+    });
+    return res.status(httpStatus.OK)
+      .json({ success: 1, message: 'Operation succeed', response });
+  } catch (err) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ success: 0, message: err })
+  }
+
+});
+
+/* Generic CRUD methods. Implement new methods above */
 baseController.setAllCrudMethods(router, Event);
 
 export default router;
