90831bc89f50f57b6cb01684b59d3e6fdf0d4d64
Vitor Martins <vitorfigmartins@gmail.com>
2020-01-19 20:57:27 -0300

    Package.json and database config correction

--- src/config/database.js
+++ src/config/database.js
@@ -27,20 +27,26 @@ export default {
     },
   },
   development: {
-    uri: 'postgres://omlaaryscxxbfr:882a70ec7ccbd02a8407c5542ae0be792398e913cff11cac19f133b3b7c5a563@ec2-54-83-201-84.compute-1.amazonaws.com:5432/d8ll4o1ibiui0b',
-    host: 'ec2-54-83-201-84.compute-1.amazonaws.com',
+    uri: 'postgres://xncnzcvxhmllnl:768ff59024f4ccf4edecfa48af2fe4500b34e74d174352a4ca68a7673f97f2cd@ec2-174-129-234-111.compute-1.amazonaws.com:5432/d79vlv570h31q1',
+    host: 'ec2-174-129-234-111.compute-1.amazonaws.com',
     port: 5432,
     database: 'd8ll4o1ibiui0b',
-    username: 'omlaaryscxxbfr',
-    password: '882a70ec7ccbd02a8407c5542ae0be792398e913cff11cac19f133b3b7c5a563',
+    username: 'xncnzcvxhmllnl',
+    password: '768ff59024f4ccf4edecfa48af2fe4500b34e74d174352a4ca68a7673f97f2cd',
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-      ssl: true
+      ssl: false
     },
     define: {
-      timestamps: false,
+      timestamps: true,
       freezeTableName: true,
+      createdAt: 'dh_insertion',
+      updatedAt: false,
+      defaultScope: {
+        attributes: { exclude: ['insertionUserId', 'dh_insertion'] },
+      },
+      scopes: {}
     },
     pool: {
       max: 5,
