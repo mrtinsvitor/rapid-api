18321bf0d43c8c80755ee4b1b7206f5107088dd2
Vitor Martins <vitorfigmartins@gmail.com>
2020-02-04 19:12:35 -0300

    Moving database port to .env file

--- src/config/database.js
+++ src/config/database.js
@@ -30,7 +30,7 @@ export default {
   },
   development: {
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
