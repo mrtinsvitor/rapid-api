446c511883d3e10e4e71954e6544ea0d86bd97d3
Vitor Martins <vitorfigmartins@gmail.com>
2020-02-29 19:01:43 -0300

    Adding validation error handling

--- src/helpers/ErrorHandler.js
+++ src/helpers/ErrorHandler.js
@@ -1,11 +1,15 @@
 import httpStatus from 'http-status';
 
+import { handleValidationError } from './ValidationError';
+
 class ErrorHandler extends Error {
   constructor(
+    type = 'unknow_error',
     message = 'An error has occurred. Try again later.',
     statusCode = httpStatus.INTERNAL_SERVER_ERROR
   ) {
     super();
+    this.type = type;
     this.message = message;
     this.statusCode = statusCode;
     this.status = httpStatus['500_NAME'];
@@ -22,7 +26,12 @@ export const handleError = (err, res) => {
       status: httpStatus[`${statusCode}_NAME`]
     });
   }
-  console.log(`[${new Date()}][Internal Error]: ${err}`)
+
+  if (err.errors !== null && err.errors[0].type === 'Validation error') {
+    return handleValidationError(err, res);
+  }
+
+  console.log(`[${new Date()}][Internal Error]: ${err}`);
   return res.status(httpStatus.INTERNAL_SERVER_ERROR)
     .json(new ErrorHandler());
 
---
+++ src/helpers/ValidationError.js
@@ -0,0 +1,23 @@
+import httpStatus from 'http-status';
+
+class ValidationError extends Error {
+  constructor(invalidFields) {
+    super();
+    this.type = 'validation_error';
+    this.invalidFields = invalidFields;
+    this.statusCode = httpStatus.BAD_REQUEST;
+    this.status = httpStatus['400_NAME'];
+  }
+}
+
+export const handleValidationError = (err, res) => {
+  const invalidFields = err.errors.map(error => {
+    console.log(`[${new Date()}][Validation Error]: ${error.message}`);
+    return { field: error.path, reason: error.message };
+  });
+
+  return res.status(httpStatus.BAD_REQUEST)
+    .json(new ValidationError(invalidFields));
+};
+
+export default ValidationError;
